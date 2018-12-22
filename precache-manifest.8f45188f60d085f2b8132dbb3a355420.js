fd204c5e807350c4aa89153a201e806ea1f43fec
hilbo <hardimanhk@gmail.com>
2018-12-22 16:34:57 -0500

    Updates

---
+++ precache-manifest.8f45188f60d085f2b8132dbb3a355420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "dc31ee24bbbcf83c7b7c",
+    "url": "/memory-game/static/js/main.dc31ee24.chunk.js"
+  },
+  {
+    "revision": "1c7ebbc99c49b9e8bf35",
+    "url": "/memory-game/static/js/1.1c7ebbc9.chunk.js"
+  },
+  {
+    "revision": "dc31ee24bbbcf83c7b7c",
+    "url": "/memory-game/static/css/main.65243002.chunk.css"
+  },
+  {
+    "revision": "085fdaadb5e15e5d84dc9152ac75df5d",
+    "url": "/memory-game/index.html"
+  }
+];
