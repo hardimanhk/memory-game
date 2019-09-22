fee98a489eac4e424d2f8f82a2c9924dc76836de
hilbo <hardimanhk@gmail.com>
2019-09-22 18:50:43 -0400

    Updates

--- precache-manifest.7d1ffcf0e37bd93af5ee6223f6b8de26.js
+++ precache-manifest.7d1ffcf0e37bd93af5ee6223f6b8de26.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/memory-game/static/js/runtime~main.66c34b29.js"
   },
   {
-    "revision": "4bb0e7feb96bef3c1d29",
-    "url": "/memory-game/static/js/main.4bb0e7fe.chunk.js"
+    "revision": "4c9666d734be0775c480",
+    "url": "/memory-game/static/js/main.4c9666d7.chunk.js"
   },
   {
     "revision": "1c7ebbc99c49b9e8bf35",
     "url": "/memory-game/static/js/1.1c7ebbc9.chunk.js"
   },
   {
-    "revision": "4bb0e7feb96bef3c1d29",
+    "revision": "4c9666d734be0775c480",
     "url": "/memory-game/static/css/main.c170306f.chunk.css"
   },
   {
-    "revision": "defdde87839a5aa14df1b903432002a5",
+    "revision": "aebaab3193f74e816f4111bbfe3746f4",
     "url": "/memory-game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/memory-game/precache-manifest.936dea2f9e454b49a6a479c33907523a.js"
+  "/memory-game/precache-manifest.7d1ffcf0e37bd93af5ee6223f6b8de26.js"
 );
 
 workbox.clientsClaim();
